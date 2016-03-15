faadbf9d3cbd9f7d72f2cdeac3fb14baccd9b773
Jesse Shawl <jesse@jshawl.com>
2016-03-15 09:10:27 -0400

    reset after looping

--- .build.sh
+++ .build.sh
@@ -14,10 +14,11 @@ for tag in `git tag`; do
   gitbook install > /dev/null
   gitbook build > /dev/null
   cp -r _book/* dist/$tag/
-  git reset --hard $latest
-  cd dist
-  git init
-  git add .
-  git commit -m "built from $latest"
-  git push -f git@github.com:ga-dc/fundamentals.git master:gh-pages
 done;
+
+cd dist
+git init
+git add .
+git commit -m "built from $latest"
+git push -f git@github.com:ga-dc/fundamentals.git master:gh-pages
+git reset --hard $latest
