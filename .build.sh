662a13ded245ab1a1cfc82875cecee9a35e28858
Jesse Shawl <jesse@jshawl.com>
2016-03-15 08:50:26 -0400

    push to gh-pages

--- .build.sh
+++ .build.sh
@@ -13,4 +13,9 @@ for tag in `git tag`; do
   rm -r _book
   cp -r _book/* dist/$tag/
   git reset --hard $latest
+  cd dist
+  git init
+  git add .
+  git commit -m "built from $latest"
+  git push -f origin gh-pages
 done;
