5542928bbf1b76b09909fdbf1b0a7f94e840ade3
Jesse Shawl <jesse@jshawl.com>
2016-04-12 10:51:27 -0400

    add travis update

--- .deploy.sh
+++ .deploy.sh
@@ -4,13 +4,11 @@ set -o errexit -o nounset
 
 rev=$(git rev-parse --short HEAD)
 
-GH_TOKEN=`cat ~/.github`
-
 cd ../dist
 
 git init
-git config user.name "JD Maresco"
-git config user.email "jdmaresco@gmail.com"
+git config user.name "Jesse Shawl"
+git config user.email "jesse@jshawl.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/ga-dc/fundamentals.git"
 git fetch upstream
