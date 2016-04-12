c8fd56973327370dd2aff41e3f2c15ea05ee3924
Jesse Shawl <jesse@jshawl.com>
2016-04-12 09:07:16 -0400

    hide output

--- .build.sh
+++ .build.sh
@@ -12,12 +12,15 @@ mkdir dist
 for tag in `git tag`; do
   mkdir dist/$tag
   git reset --hard $tag
+  echo "Building $tag"
   touch _book && rm -r _book
-  gitbook install 
-  gitbook build  
+  gitbook install > /dev/null
+  gitbook build > /dev/null
   cp -r _book/* dist/$tag/
   last_tag=$tag
 done;
+
+cp -r _book/* dist/
 
 echo "<meta http-equiv='refresh' content='0;URL=./$last_tag'>" >> dist/index.html
 
