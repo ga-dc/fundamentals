e13f43be3f74e9ba919d79373f2544860389f17d
Jesse Shawl <jesse@jshawl.com>
2016-04-12 11:31:19 -0400

    remove origin/

--- .build.sh
+++ .build.sh
@@ -24,7 +24,7 @@ for tag in $branches; do
   if [ "$tag" != "gh-pages" ]; then
     mkdir ../dist/$tag
     echo "Building $tag"
-    git reset --hard origin/$tag
+    git reset --hard $tag
     touch _book && rm -r _book
     gitbook install > /dev/null && gitbook build > /dev/null && cp -r _book/* ../dist/$tag/
   fi
